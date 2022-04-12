c89211524c1288d0436fa0fb040a4abad7515af7
zelkova19 <cohiba666@gmail.com>
2022-04-12 12:19:18 +0300

    homework3

--- homework1/homework1.py
+++
@@ -1,1 +0,0 @@
-print("Homework Example 'Hello World'")
--- homework2/src/Circle.py
+++
@@ -1,9 +0,0 @@
-from homework2.src.Figure import Figure
-
-
-class Circle(Figure):
-    def __init__(self, r):
-        self.r = r
-        self.__area = int(3.14 * r ** 2)
-        self.__perimeter = int(2 * 3.14 * r)
-        super().__init__("Круг", self.__area, self.__perimeter)
